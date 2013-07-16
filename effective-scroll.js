1c248bcf38e2b609eb001746b332aee9c2856a94
grandsopikat <kalaomer@hotmail.com>
2013-07-16 17:36:53 +0300

    Update effective-scroll.js

--- effective-scroll.js
+++ effective-scroll.js
@@ -1,28 +1,28 @@
 $.fn.effectiveScroll = function( options ) {
 
-  	var $scrollContent = $( this );
-		var opt = $.extend(
-			options,
-			{
-				animateSpeed: 300,
-				callBack: function() {}
-			}
-		);
+	var $scrollContent = $( this );
+	var opt = $.extend(
+		options,
+		{
+			animateSpeed: 300,
+			callBack: function() {}
+		}
+	);
 
-		$('a[href^="#"]').bind("click", function() {
-			var $this = $( this );
-			var $target = $( $this.attr("href") );
+	$('a[href^="#"]').bind("click", function() {
+		var $this = $( this );
+		var $target = $( $this.attr("href") );
 
-			if ( $this.attr("href") == "#" || $target == [] ) {
-				return false;
-			}
-
-			var top = $target.offset().top - $scrollContent.offset().top;
-
-			$scrollContent.animate( { scrollTop: "+=" + top }, opt.animateSpeed, function() {
-				window.location.hash = $target.attr("id");
-			} );
-
+		if ( $this.attr("href") == "#" || $target == [] ) {
 			return false;
-		});
-	};
+		}
+
+		var top = $target.offset().top - $scrollContent.offset().top;
+
+		$scrollContent.animate( { scrollTop: "+=" + top }, opt.animateSpeed, function() {
+			window.location.hash = $target.attr("id");
+		} );
+
+		return false;
+	});
+};
